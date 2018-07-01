811ecf915cac424f1dc626e5d9611f7fc064eff0
hyota <much.ado.about.nothig.1598.1599@gmail.com>
2018-07-01 10:52:11 +0900

    パッケージ作成

--- app/src/main/java/com/github/hyota/asciiartboardreader/presentation/main/MainActivity.java
+++ app/src/main/java/com/github/hyota/asciiartboardreader/presentation/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.github.hyota.asciiartboardreader;
+package com.github.hyota.asciiartboardreader.presentation.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.hyota.asciiartboardreader.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
